4c24d9da6815565944f4516e0a6274b7e0e219fa
cardonju <60967681+cardonju@users.noreply.github.com>
2020-04-09 13:21:49 -0400

    Update Proj2_2.py

--- Proj2_2.py
+++ Proj2_2.py
@@ -1,10 +1,10 @@
 import sqlalchemy as db
 from sqlalchemy import create_engine
 
-#trying to create an engine to connect to a database.  in this case the database for 
-#CPS3740 class it requieres the database user information and password as well as the
+#trying to create an engine to connect to a database.  in this case the database  
+# it requieres the database user information and password as well as the
 #mysql port
-engine = create_engine('mysql://cardonju:imc.kean.edu:3306/CPS3740')
+engine = create_engine('')
 
 connection = engine.connect()
 metadata = db.MetaData()
